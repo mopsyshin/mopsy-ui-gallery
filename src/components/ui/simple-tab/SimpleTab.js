7c676ae51bb0b16e157eba21f69afef59401680b
mopsyshin <mopsyshin@gmail.com>
2019-09-02 12:03:09 +0900

    simple tab work

--- src/components/ui/simple-tab/SimpleTab.js
+++ src/components/ui/simple-tab/SimpleTab.js
@@ -1,18 +1,26 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import './SimpleTab.scss';
+import tabs from './tab.config';
 
 const SimpleTab = props => {
+  const tabMenus = tabs;
 
   const renderTabItems = () => {
-    return <TabItem/>
+    return tabMenus.map((tab, index) => {
+      return <TabItem data={tab} key={index}/>
+    }) 
+  }
+
+  const indicatorWidth = () => {
+    
   }
 
   return (
     <div className="simple-tab-container">
       <div className="tab-header">
         {renderTabItems()}
-        <div className="tab-indicator"></div>
+        <div className="tab-indicator" style={{width: indicatorWidth}}></div>
       </div>
       <div className="tab-body">
         <Route path="/ui/simple-tab/:tabname" component={TabPage}/>
@@ -23,7 +31,9 @@ const SimpleTab = props => {
 
 const TabItem = props => {
   return (
-    <div className="tab-item">tab item 1</div>
+    <div className="tab-item">
+      {props.data.tabName}
+    </div>
   )
 }
 
---
+++ src/components/ui/simple-tab/tab.config.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    tabName: 'Tab name1',
+    isActive: false,
+  },
+  {
+    tabName: 'Tab name2',
+    isActive: false,
+  },
+  {
+    tabName: 'Tab name3',
+    isActive: false,
+  },
+  {
+    tabName: 'Tab name4',
+    isActive: false,
+  },
+  {
+    tabName: 'Tab name5',
+    isActive: false,
+  }
+]
