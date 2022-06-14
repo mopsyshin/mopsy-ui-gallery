dbd3418fee5ffdca9a26c24fbb25a5555d2bff66
mopsyshin <mopsyshin@gmail.com>
2022-06-14 18:16:01 +0900

    dropdown refactoring

--- src/components/ui/dropdown/Dropdown.js
+++ src/components/ui/dropdown/Dropdown.js
@@ -1,96 +1,65 @@
-import React, { useState, useEffect, useContext, useCallback } from 'react';
-import { observer } from 'mobx-react-lite';
-import { UiContext } from 'stores';
-import './Dropdown.scss';
-import IcArrow from 'assets/IcArrow';
-import data from '../../../dummy/dropdownSample';
+import React, { useContext } from "react";
+import { observer } from "mobx-react-lite";
+import { UiContext } from "stores";
+import "./Dropdown.scss";
+import IcArrow from "assets/IcArrow";
+import data from "../../../dummy/dropdownSample";
+import classNames from "classnames/dedupe";
+import useDropdown from "./useDropdown";
 
-
-const Dropdown = props => {
+const Dropdown = () => {
   const store = useContext(UiContext);
 
-  const [selecting, setSelecting] = useState(false);
-  const [selectedItem, setSelectedItem] = useState(data.arr[0]);
-
-  const toggleDropdown = () => {
-    if (selecting) {
-      setSelecting(false);
-      store.addLog(`[Dropdown Menu : Toggle Dropdown] false`);
-    } else {
-      setSelecting(true);
-      store.addLog(`[Dropdown Menu : Toggle Dropdown] true`);
-    }
-  }
-
-  const selectItem = useCallback(item => {
-    document.body.style.overflow = 'unset';
-    setSelecting(false);
-    setSelectedItem(item);
-    store.addLog(`[Dropdown Menu : Select Item] ${item.value}, ${item.display}`);
-  }, [store])
-
-  const setBodyOverflow = status => {
-    if (status) {
-      document.body.style.overflow = 'unset';
-    } else {
-      document.body.style.overflow = 'hidden';
-    }
-  }
-
-  useEffect(() => {
-    if (data.initItem) {
-      selectItem(data.initItem);
-    }
-  }, [selectItem]);
-
-  const wrapperHeight = () => {
-    if (selecting) {
-      return data.arr.length > 5 ? (56 * 5 + 28) + 'px' : (56 * (data.arr.length + 1)) + 'px';
-    }
-    return '56px';
-  }
-
-  const dropdownList = () => {
-    if (selecting) {
-      return (
-        <div onMouseEnter={() => setBodyOverflow(false)}
-             onMouseLeave={() => setBodyOverflow(true)}>
-        {
-          data.arr.map((item, index) => {
-            return (
-              <div key={index}>
-                <div></div>
-                <div className="display-item" onClick={() => {selectItem(item)}} >
-                  {item.display}
-                </div>
-              </div>
-            )
-          })
-        }
-        </div>
-      )
-    }
-  }
+  const {
+    selecting,
+    wrapperHeight,
+    toggleDropdown,
+    selectedItem,
+    setBodyOverflow,
+    selectItem,
+  } = useDropdown({ store });
 
   return (
     <div className="dropdown-container">
-      <div className={`wrapper ${selecting ? 'selecting' : ''}`}>
-        <div className={`scroll-view ${selecting ? 'selecting' : ''}`} style={{
-          height: wrapperHeight()
-        }}>
+      <div className={classNames("wrapper", { selecting })}>
+        <div
+          className={classNames("scroll-view", { selecting })}
+          style={{
+            height: wrapperHeight(),
+          }}
+        >
           <div className="display-item" onClick={toggleDropdown}>
-            <div>
-              {selectedItem.display}
-            </div>
-            <div className={`ic-wrapper ${selecting ? 'selecting' : ''}`}>
-              <IcArrow/>
+            <div>{selectedItem.display}</div>
+            <div className={classNames("ic-wrapper", { selecting })}>
+              <IcArrow />
             </div>
           </div>
-          { dropdownList() }
+          {selecting && (
+            <div
+              onMouseEnter={() => setBodyOverflow(false)}
+              onMouseLeave={() => setBodyOverflow(true)}
+            >
+              {data.arr.map((item, index) => {
+                return (
+                  <div key={index}>
+                    <div></div>
+                    <div
+                      className="display-item"
+                      onClick={() => {
+                        selectItem(item);
+                      }}
+                    >
+                      {item.display}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default observer(Dropdown);
---
+++ src/components/ui/dropdown/useDropdown.js
@@ -0,0 +1,62 @@
+import { useState, useEffect, useCallback } from "react";
+import data from "../../../dummy/dropdownSample";
+
+const useDropdown = ({ store }) => {
+  const [selecting, setSelecting] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(data.arr[0]);
+
+  const toggleDropdown = () => {
+    if (selecting) {
+      setSelecting(false);
+      store.addLog(`[Dropdown Menu : Toggle Dropdown] false`);
+    } else {
+      setSelecting(true);
+      store.addLog(`[Dropdown Menu : Toggle Dropdown] true`);
+    }
+  };
+
+  const selectItem = useCallback(
+    (item) => {
+      document.body.style.overflow = "unset";
+      setSelecting(false);
+      setSelectedItem(item);
+      store.addLog(
+        `[Dropdown Menu : Select Item] ${item.value}, ${item.display}`
+      );
+    },
+    [store]
+  );
+
+  const setBodyOverflow = (status) => {
+    if (status) {
+      document.body.style.overflow = "unset";
+    } else {
+      document.body.style.overflow = "hidden";
+    }
+  };
+
+  useEffect(() => {
+    if (data.initItem) {
+      selectItem(data.initItem);
+    }
+  }, [selectItem]);
+
+  const wrapperHeight = () => {
+    if (selecting) {
+      return data.arr.length > 5
+        ? 56 * 5 + 28 + "px"
+        : 56 * (data.arr.length + 1) + "px";
+    }
+    return "56px";
+  };
+  return {
+    selecting,
+    wrapperHeight,
+    toggleDropdown,
+    selectedItem,
+    setBodyOverflow,
+    selectItem,
+  };
+};
+
+export default useDropdown;
